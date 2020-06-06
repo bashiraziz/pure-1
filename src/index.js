312db25af50c9a6900ee460ffef9a62e0cca04d1
bashiraziz <bashiraziz@yahoo.com>
2020-06-06 19:00:58 -0400

    update app.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./index.css";
+import ReactDOM from "react-dom";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+
